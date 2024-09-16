696e31869538ea27a2649ecda604c7e5792457b6
Ustabaev Umidjon <ustabaevumidjon@nba-066-01b-uz.local>
2024-09-16 15:56:31 +0500

    Remove answers

--- SQL Deep Dive/3 Valued Logic Exercises/questions.sql
+++ SQL Deep Dive/3 Valued Logic Exercises/questions.sql
@@ -4,8 +4,6 @@
 * Table: customers
 * Question: adjust the following query to display the null values as "No Address"
 */
-SELECT address2 
-FROM customers
 
 /*
 * DB: Store
@@ -13,15 +11,9 @@ FROM customers
 * Question: Fix the following query to apply proper 3VL
 */
 
-SELECT *
-FROM customers
-WHERE COALESCE(address2, null) IS NOT null;
 
 /*
 * DB: Store
 * Table: customers
 * Question: Fix the following query to apply proper 3VL
 */
-
-SELECT coalesce(lastName, 'Empty'), * from customers
-where (age = null);
